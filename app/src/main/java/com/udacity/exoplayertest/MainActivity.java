04aecf01b80935434f933a8836c10c8039fe716f
Federico Creti <federico.creti87@outlook.it>
2018-03-30 08:26:39 +0200

    First commit

---
+++ app/src/main/java/com/udacity/exoplayertest/MainActivity.java
@@ -0,0 +1,119 @@
+package com.udacity.exoplayertest;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+public class MainActivity extends AppCompatActivity implements Player.EventListener {
+
+    private SimpleExoPlayer simpleExoPlayer;
+    private SimpleExoPlayerView simpleExoPlayerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*simpleExoPlayerView = findViewById(R.id.playerView);
+        initializePlayer();
+        try {
+            setMediaAndPlay(Uri.parse("https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd974_-intro-creampie/-intro-creampie.mp4"), this);
+        } catch (Exception ex){
+
+        }*/
+    }
+
+    public void initializePlayer(){
+        if (simpleExoPlayer == null){
+            simpleExoPlayerView.requestFocus();
+
+            //BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+            //TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+            TrackSelector trackSelector = new DefaultTrackSelector();
+
+            LoadControl loadControl = new DefaultLoadControl();
+
+            //simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector, loadControl);
+            simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this), trackSelector, loadControl);
+            simpleExoPlayerView.setPlayer(simpleExoPlayer);
+
+            simpleExoPlayer.addListener(this);
+        }
+    }
+
+    public void setMediaAndPlay(Uri uri, Context context) throws Exception{
+        if (simpleExoPlayer == null) {
+            throw new Exception("ExoPlayer must be initialized before it's use. Use initializePlayer(simpleExoPlayerView)");
+        }
+        String userAgent = Util.getUserAgent(context, "ExoPlayerTest");
+
+        MediaSource mediaSource = new ExtractorMediaSource(uri,
+                new DefaultDataSourceFactory(context, userAgent),
+                new DefaultExtractorsFactory(), null, null);
+
+        simpleExoPlayer.prepare(mediaSource);
+        simpleExoPlayer.setPlayWhenReady(true);
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity() {
+
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+    }
+}
